4cbf89e60a8a34dad0729ccc28269e322fa8eb13
Jovi <jovimdl@gmail.com>
2017-06-09 02:43:08 -0700

    Progress 2

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+int data[4] = {2, 1, 4, 3};                     //Data Array
+int tmp[4];                                     //Tmp Array
+
+int n = sizeof(data)/sizeof(int);               //Size of Array
+
+void mergesort(left, mid, right){
+
+int tmp = left;
+  if(n=2){
+    if(right<left){
+      tmp = data[left];
+      data[left] = data[right];
+      data[right] = tmp;
+      return;
+    }else{
+      return;
+    }
+  }
+
+}
+
 int main(int argc, char const *argv[]) {
-
-  int data[4] = {2, 1, 4, 3};                     //Data Array
-  int tmp[4];                                     //Tmp Array
-
-  int n = sizeof(data)/sizeof(int);               //Size of Array
 
   int mid = n/2;                                  //Mid Index
   int left = 0;                                   //Left Index
@@ -16,13 +32,17 @@ int main(int argc, char const *argv[]) {
   printf("LEFT: %d\n", left);
   printf("MID: %d\n", mid);
   printf("RIGHT: %d\n", right);
-
+  printf("\nOriginal Array: ");
   for(i=0; i<n; i++){
     printf("%d", data[i]);
   }
 
-  while(tmpRight <= right){
+  mergesort(left, mid, right);
 
+  printf("\nSorted Array: ");
+
+  for(i=0; i<n; i++){
+    printf("%d ", data[i]);
   }
 
   return 0;
